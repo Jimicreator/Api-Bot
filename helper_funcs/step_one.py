e9f3cc96a5eecda57ad3da5000f640793a76f1be
Mehmetbaba06 <83150866+Mehmetbaba06@users.noreply.github.com>
2021-06-01 16:35:18 +0300

    Update step_one.py

--- helper_funcs/step_one.py
+++ helper_funcs/step_one.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-""" STEP ONE """
+""" BİR ADIM """
 
 import requests
 
